5ee95cc789392905984bf7084eb777b1577ed54d
jrbalderrama <jrbalderrama@users.noreply.github.com>
2018-10-24 11:34:54 +0200

    add minimal example of CLI

--- allocine.py
+++ allocine.py
@@ -1,13 +1,14 @@
-import hashlib
 from base64 import b64encode
-try: # python3
-	from urllib.parse import urlencode
-except: #python2
-	from urllib import urlencode
-import requests
-import time
+from collections import defaultdict
+from datetime import date, datetime
+from urllib.parse import urlencode
+import hashlib
 import json
-from pprint import pprint # for a more readable json output
+import time
+import sys
+
+import requests
+
 
 ALLOCINE_BASE_URL = "http://api.allocine.fr/rest/v3/"
 ALLOCINE_PARTNER_KEY = '100043982026'
@@ -260,3 +261,130 @@ def episode(code, profile=None, mediafmt=None, format="json", striptags=None):
     if striptags is not None:
         data["striptags"] = striptags
     return do_request("episode", data)
+
+
+def map_lang(code):
+    languages = {
+        6001: 'français',
+        6002: 'English',
+        6003: 'español',
+        6004: "italiano",
+        6006: 'deutsch',
+        6008: 'Chinese',
+        6009: 'Hindi',
+        6011: 'Arab',
+        6015: 'Danish',
+        6019: 'Hebrew',
+        6021: 'Japanese',
+        6027: 'Portuguese',
+        6026: 'Polish',
+        6030: 'Russian', #to confirm
+        6033: 'Swedish',
+        6036: 'Turkish',
+        6040: 'Korean',
+        6053: 'Icelandic',
+        6055: 'Finnish',
+        6104: 'Ukrainian'
+    }
+    return languages.get(code, code)
+
+
+MY_LANGS = [6002, 6003, 6004, 6006]
+
+
+def map_theatre(cinema):
+    theatre = {
+        'arvor': 'P0084',
+        'cineville': 'P0085',
+        'colombier': 'P0085',
+        'tnb': 'P0102',
+        'gaumont': 'P0091'}
+    return theatre.get(cinema, cinema)
+
+
+def to_str(runtime):
+    total_seconds: int = int(runtime)
+    hours: int = total_seconds // 3600
+    minutes: int = total_seconds // 60 % 60
+    seconds: int = total_seconds % 60
+    return '{:02}:{:02}:{:02}'.format(hours, minutes, seconds)
+
+
+def week_planning(planning):
+    schedule = defaultdict(list)
+    for week_day in planning:
+        day_ = week_day['d']
+        for slot in week_day['t']:
+            schedule[day_].append(slot['$'])
+    return schedule
+
+
+def today_planning(planning):
+    today_hours = [hours for day, hours in week_planning(planning).items()
+                   if day == date.today().isoformat()][0]
+    current_hour = datetime.now().hour
+    return [hour for hour in today_hours
+            if datetime.strptime(hour, '%H:%M').hour > current_hour]
+
+
+def lookup_original_name(code):
+    return movie(code, filter='movie')['movie']['originalTitle']
+
+
+def to_stars(rating):
+    stars = int(rating) % 5 + (1 if int(rating * 100) % 100 >= 51  else 0)
+    return ''.join(['☆' for x in range(stars)])
+
+
+
+if __name__ == "__main__":
+    theatre = map_theatre(sys.argv[1])
+    #theatre = 'P0787' #'C0100'
+    show_week = len(sys.argv) > 2
+    my_date = None if show_week else date.today().isoformat()
+    movie_list = showtimelist(theaters=theatre, date=my_date) \
+        ['feed']['theaterShowtimes'][0]['movieShowtimes']
+    for my_movie in movie_list:
+        release = my_movie['releaseWeek']
+        week_release = '·' if release == 'true' else '×'
+        language = my_movie['version']['lang']
+        planning = my_movie['scr']
+        information = my_movie['onShow']['movie']
+        code = information['code']
+        title = information['title']
+        genre = information['genre'][0]['$']
+        live = True if 'Pathé' in title else False
+        directors = information.get('castingShort', {})
+        directors = directors.get('directors', '')
+        runtime = information.get('runtime', 0)
+        rating = information['statistics'].get('pressRating', 0)
+        fans = information['statistics'].get('userRating', 0)
+        trailer = information['link'][0]['href']
+        if not live:
+            if language in MY_LANGS:
+                title = lookup_original_name(code)
+
+            rating_constellation = to_stars(rating)
+            fans_constellation = to_stars(fans)
+            separator = '|' \
+                if rating_constellation and fans_constellation else ''
+            print('{} {}, {} ({}) {}{}{}'.format(
+                week_release,
+                title,
+                genre,
+                map_lang(language),
+                '{}'.format(rating_constellation),
+                separator,
+                '{}'.format(fans_constellation)))
+            if show_week:
+                for day, hours in week_planning(planning).items():
+                    week_day = datetime.strptime(day, '%Y-%m-%d').date()
+                    print('\t\t{}: {}'.format(week_day.strftime('%A %b, %d'),
+                                              ', '.join(hours)))
+
+            else:
+                planning = today_planning(planning)
+                if planning:
+                    print('\t\t{}'.format(', '.join(planning)))
+
+        # TODO async for foreing movies
